e25919b67f5d4e00266d20a8012b63f0b652ae35
juanpablososa <85967860+juanpablososa@users.noreply.github.com>
2022-03-29 11:20:42 -0500

    Update consulta_clientes.sql

--- consulta_clientes.sql
+++ consulta_clientes.sql
@@ -1,5 +1,8 @@
+
 select DISTINCT c.documento, c.primer nombre, c.apellido
 from cliente c
 inner join factura f on f.documento_nit = c.documento_nit
 where f.total = 100.000
-and f.fecha = 
+and f.fecha = 60 
+ORDER BY ;
+
